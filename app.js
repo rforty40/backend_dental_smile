b352d2d3141bcc1a031ad27098701d2f02fd0d7a
RonnyForty <fortyservidor99@hotmail.com>
2023-06-23 01:04:46 -0500

    cambio de query en la conexion a la bd

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.use(authRoutes);
 //verificar conexion con la BD antes de abrir el puerto del servidor
 try {
   await poolDB.query("SET @@global.time_zone = '-05:00';");
-  const { data } = await poolDB.query("SELECT current_timestamp();");
-  console.log(data);
+  const result = await poolDB.query("SELECT current_timestamp();");
+  console.log(result);
   console.log("conexion exitosa con BD");
   app.listen(PORT);
   console.log(`Server is listening on port ${PORT}`);
